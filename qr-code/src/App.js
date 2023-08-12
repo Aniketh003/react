b1cd602d92b66534f66bf1c66c3f8dda0f2055da
Aniketh003 <21wj1a6912@gniindia.org>
2023-08-12 14:31:02 +0530

    f

--- qr-code/src/App.js
+++ qr-code/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const generateQR = (url, size) => {
+  return `https://api.qrserver.com/v1/create-qr-code/?size=${size ? size : "300x300"}&data=${url}`
+}
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [size, setSize] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="qr-container">
+        <h1 className='qr-title'>QR Code Generator</h1>
+        <div className="input-container">
+            <label className='input-label'>QR URL:</label>
+            <input className='input-feild' type="text" placeholder='enter the url' value={url} onChange={(e) => setUrl(e.target.value)} />
+        </div>
+        <div className="input-container">
+            <label className='input-label'>QR SIZE:</label>
+            <input className='input-feild' type="text" placeholder='example (150x150)' value={size} onChange={(e) => setSize(e.target.value)} />
+        </div>
+        <div className="qr-img">
+        {
+          url ? 
+                <img src={generateQR(url, size)} className='qr-image' alt='QR Code' /> : <></>
+        }
+        </div>
+      </div>
   );
 }
 
